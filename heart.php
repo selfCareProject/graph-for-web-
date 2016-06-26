816952fc4642c0579a9b4242ded6544f963f4bd6
sasooShams <sasooShams@users.noreply.github.com>
2016-06-26 20:48:48 +0200

    Add files via upload

---
+++ heart.php
@@ -0,0 +1,28 @@
+<?php
+include 'varibles.php';
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+$sql = " INSERT INTO `u787994640_care`.`projections_sample` (`revenue`) VALUES ('40')
+";
+$email ="s12@yahoo.com";
+//$resultss = $conn->query($sql);
+$sth = "SELECT Heart FROM(SELECT * FROM patient_records WHERE Patient_email ='$email'  ORDER BY Date DESC  LIMIT 12) X  ORDER BY Date ASC";
+$resultss = $conn->query($sth);
+
+//if ($resultss=mysqli_query($conn,$sth))
+$rows = array();
+$rows['name'] = 'Heart';
+while($r = $resultss->fetch_assoc()) {
+   // $rows['data'][] = $r['revenue'];
+     $rows['data'][] = $r['Heart'];
+}
+$result = array();
+array_push($result,$rows);
+//array_push($result,$rows1);
+
+
+print json_encode($result, JSON_NUMERIC_CHECK);
+
+
+?>
